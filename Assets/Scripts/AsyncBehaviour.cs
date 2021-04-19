2c1f6d70d35343d939f1896056ccff1e51a1317e
Jannik Busse <jannik@vrtxlabs.com>
2021-04-19 12:22:13 +0200

    Use Stylecop Rules

--- Assets/Scripts/AsyncBehaviour.cs
+++ Assets/Scripts/AsyncBehaviour.cs
@@ -4,31 +4,35 @@ using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 
+/// <summary>
+/// Contains a simple example to show the thread IDs of several dispatched tasks. Tasks are dispatched using either await Task, Task.Run or Task.ContinueWith.
+/// In pure C# context, tasks will run on different threads. In Unity context, the same Code leads to tasks started with await Task being forced to run on the main thread.
+/// </summary>
 public class AsyncBehaviour : MonoBehaviour
 {
 
     [SerializeField]
-    private TextMeshProUGUI label = null;
+    private TextMeshProUGUI _label = null;
 
     private async void Start()
     {
         // Main thread
         string message = "";
-        message += PrintThread("Main Thread Print") + "\n";
+        message += this.PrintThread("Main Thread Print") + "\n";
 
         // Task.Run / Task.ContinueWith
-        Task<string> task1 = Task.Run(() => PrintThreadAsync("Task.Run()", 500)); // other thread
-        Task<string> task2 = task1.ContinueWith(_ => PrintThread("Continue task1")); // other thread
-        Task<string> task3 = task2.ContinueWith(_ => PrintThread("Continue task2 in current Context"), TaskScheduler.FromCurrentSynchronizationContext()); // main thread
-        Task<string> task4 = task3.ContinueWith(_ => PrintThread("Continue task3")); // other thread
+        Task<string> task1 = Task.Run(() => this.PrintThreadAsync("Task.Run()", 500)); // other thread
+        Task<string> task2 = task1.ContinueWith(_ => this.PrintThread("Continue task1")); // other thread
+        Task<string> task3 = task2.ContinueWith(_ => this.PrintThread("Continue task2 in current Context"), TaskScheduler.FromCurrentSynchronizationContext()); // main thread
+        Task<string> task4 = task3.ContinueWith(_ => this.PrintThread("Continue task3")); // other thread
 
         // Await behaviour
         List<Task<string>> taskList = new List<Task<string>>(); // All on main thread
-        taskList.Add(PrintThreadAsync("Task.WhenAll() - taskList 1", 1000));
-        taskList.Add(PrintThreadAsync("Task.WhenAll() - taskList 2", 1000));
-        taskList.Add(PrintThreadAsync("Task.WhenAll() - taskList 3", 1000));
-        taskList.Add(PrintThreadAsync("Task.WhenAll() - taskList 4", 1000));
-        taskList.Add(PrintThreadAsync("Task.WhenAll() - taskList 5", 1000));
+        taskList.Add(this.PrintThreadAsync("Task.WhenAll() - taskList 1", 1000));
+        taskList.Add(this.PrintThreadAsync("Task.WhenAll() - taskList 2", 1000));
+        taskList.Add(this.PrintThreadAsync("Task.WhenAll() - taskList 3", 1000));
+        taskList.Add(this.PrintThreadAsync("Task.WhenAll() - taskList 4", 1000));
+        taskList.Add(this.PrintThreadAsync("Task.WhenAll() - taskList 5", 1000));
 
         // Await results and print them on main thread
         await Task.WhenAll(taskList);
@@ -38,14 +42,14 @@ public class AsyncBehaviour : MonoBehaviour
         message += task4.Result + "\n";
         taskList.ForEach(taskInList => message += taskInList.Result + "\n");
 
-        label.text = message;
+        this._label.text = message;
     }
 
-    async Task<string> PrintThreadAsync(string functionName, int delay)
+    private async Task<string> PrintThreadAsync(string functionName, int delay)
     {
         await Task.Delay(delay);
 
-        return PrintThread(functionName);
+        return this.PrintThread(functionName);
     }
 
     // Get Thread data
